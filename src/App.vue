396b0336970f48fd491d5bf163de5edf25cf94e1
galaxyjiang <876708058@qq.com>
2022-05-17 16:22:59 +0800

    fix: 添加全局vue单文件组件的类型声明

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,16 @@
 <template>
-  <router-view/>
+  <router-view />
 </template>
 
-<script lang="ts">
-import {provide, ref} from "vue";
+<script lang="ts" setup>
+import { provide, ref } from "vue";
 import router from "./router";
-
-export default {
-  name: 'App',
-  setup() {
-    const width = document.documentElement.clientWidth;
-    const asideVisible = ref(width > 500);
-    router.afterEach(() => {
-      if (width <= 500) {
-        asideVisible.value = false;
-      }
-    });
-    provide('asideVisible', asideVisible);
+const width = document.documentElement.clientWidth;
+const asideVisible = ref(width > 500);
+router.afterEach(() => {
+  if (width <= 500) {
+    asideVisible.value = false;
   }
-};
+});
+provide('asideVisible', asideVisible);
 </script>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { createApp } from 'vue';
-import App from './App.vue';
+import App from '/App.vue';
 import './index.scss';
 import router from "./router/index";
 import 'virtual:svg-icons-register'
